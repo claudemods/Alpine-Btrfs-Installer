6479799eeaad448125811f8ce002287fed01fa7e
claudemods <73653396+claudemods@users.noreply.github.com>
2025-07-10 23:14:57 +0100

    lots to test might actually work this time

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/ash
 
-# Alpine Linux BTRFS Installation Script
+# Alpine Linux BTRFS Installation Script with FAT32 Boot
 # Features:
-# - BTRFS with zstd:22 compression (forced)
+# - FAT32 boot partition (for UEFI)
+# - BTRFS root with zstd:22 compression (forced)
+# - Guaranteed GRUB installation
 # - Detailed subvolume structure
-# - No swap configuration
-# - Optimized mount options
 
 # Configuration
 TARGET_DISK="/dev/sda"  # Change to your target disk
@@ -24,18 +24,18 @@ fi
 
 # Install required packages
 echo "Installing required packages..."
-apk add btrfs-progs parted e2fsprogs
+apk add btrfs-progs parted dosfstools grub grub-efi efibootmgr
 
-# Partition disk
+# Partition disk (FAT32 for boot)
 echo "Partitioning $TARGET_DISK..."
 parted -s "$TARGET_DISK" mklabel gpt
-parted -s "$TARGET_DISK" mkpart primary 1MiB 513MiB   # /boot
-parted -s "$TARGET_DISK" set 1 boot on
-parted -s "$TARGET_DISK" mkpart primary 513MiB 100%   # /
+parted -s "$TARGET_DISK" mkpart primary 1MiB 513MiB   # /boot (FAT32)
+parted -s "$TARGET_DISK" set 1 esp on                 # EFI System Partition
+parted -s "$TARGET_DISK" mkpart primary 513MiB 100%   # / (BTRFS)
 
 # Create filesystems
 echo "Creating filesystems..."
-mkfs.ext4 "${TARGET_DISK}1"
+mkfs.vfat -F32 "${TARGET_DISK}1"                     # FAT32 for boot
 mkfs.btrfs -f "${TARGET_DISK}2"
 
 # Create BTRFS subvolumes
@@ -49,12 +49,12 @@ btrfs subvolume create /mnt/@tmp
 btrfs subvolume create /mnt/@log
 btrfs subvolume create /mnt/@cache
 
-
 # Mount with specified options
 echo "Mounting with zstd:22 compression..."
 mount -o subvol=@,compress=zstd:22,compress-force=zstd:22 "${TARGET_DISK}2" /mnt
-mkdir -p /mnt/{boot,home,root,srv,tmp,var/{cache,log},var/lib/{portables,machines}}
-mount "${TARGET_DISK}1" /mnt/boot
+mkdir -p /mnt/boot/efi
+mount "${TARGET_DISK}1" /mnt/boot/efi
+mkdir -p /mnt/{home,root,srv,tmp,var/{cache,log},var/lib/{portables,machines}}
 mount -o subvol=@home,compress=zstd:22,compress-force=zstd:22 "${TARGET_DISK}2" /mnt/home
 mount -o subvol=@root,compress=zstd:22,compress-force=zstd:22 "${TARGET_DISK}2" /mnt/root
 mount -o subvol=@srv,compress=zstd:22,compress-force=zstd:22 "${TARGET_DISK}2" /mnt/srv
@@ -88,7 +88,7 @@ echo "$HOSTNAME" > /etc/hostname
 
 # Fstab configuration
 cat << EOF > /etc/fstab
-${TARGET_DISK}1 /boot ext4 rw,relatime 0 2
+${TARGET_DISK}1 /boot/efi vfat rw,relatime,fmask=0022,dmask=0022,codepage=437,iocharset=iso8859-1,shortname=mixed,errors=remount-ro 0 2
 ${TARGET_DISK}2 / btrfs rw,noatime,compress=zstd:22,compress-force=zstd:22,subvol=@ 0 1
 ${TARGET_DISK}2 /home btrfs rw,noatime,compress=zstd:22,compress-force=zstd:22,subvol=@home 0 2
 ${TARGET_DISK}2 /root btrfs rw,noatime,compress=zstd:22,compress-force=zstd:22,subvol=@root 0 2
@@ -98,18 +98,29 @@ ${TARGET_DISK}2 /var/log btrfs rw,noatime,compress=zstd:22,compress-force=zstd:2
 ${TARGET_DISK}2 /var/cache btrfs rw,noatime,compress=zstd:22,compress-force=zstd:22,subvol=@cache 0 2
 EOF
 
+# Install GRUB properly
+echo "Installing GRUB..."
+apk add grub grub-efi efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=alpine --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+
 # Additional setup
 apk add sudo
 echo '%wheel ALL=(ALL) ALL' > /etc/sudoers.d/wheel
+
+# Ensure kernel is properly configured
+echo "Configuring kernel..."
+apk add linux-lts
+mkinitfs -b / $(ls /lib/modules)
 
 # Cleanup
 rm /setup-chroot.sh
 CHROOT
 
-# Execute chroot script
-chmod +x /mnt/setup-chroot.sh
-chroot /mnt /setup-chroot.sh
 
 # Cleanup
 umount -R /mnt
-echo "Installation complete! Reboot into your new system."
+echo "Installation complete! Important notes:"
+echo "1. Ensure your BIOS is set to UEFI mode (not legacy/CSM)"
+echo "2. The bootloader is installed at /boot/efi"
+echo "3. Reboot and select Alpine Linux from your BIOS boot menu"
